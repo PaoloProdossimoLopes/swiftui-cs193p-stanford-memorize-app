068b03d7fa7835030708e6a6c316323817c7d7db
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-02-20 22:07:26 -0300

    FEATURE (GAME): CREATE ACARDS AND GRID
    DESCRIPTION:
    - CREATE A CUSTOM CARDS WITH EMOJIS
    - APPLY BORDES
    - CREATE SOME LOGIC TO SHOW AND HIDE RESUTLT
    - CREATE A BUTTON AT FOOTER SECTION
    - USING A LAZY V GRID TO CREATE A GRID

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,14 +8,100 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var numberOfCards: Int = 4
+    @State var emojis = [
+        "🐈", "🐶", "🌲", "🌵", "🦥", "🐓", "🦦", "🦜", "🦩", "🍄",
+        "🦚", "🦨", "🌾", "🐕‍🦺", "🦮", "🐚", "🦡", "🪵", "🐁"
+    ].shuffled()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            ScrollView(.vertical, showsIndicators: false) {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
+                    let piceOfArray = emojis[0..<numberOfCards]
+                    ForEach(piceOfArray, id: \.self) { emoji in
+                        CardView(icon: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            
+            Spacer()
+            
+            HStack(spacing: 50) {
+                //remove
+                createButton("minus.square", size: 40, action: removeCard)
+                
+                //Shuffle
+                createButton("rectangle.2.swap", size: 50, action: shuffleCards)
+                
+                //Add
+                createButton("plus.square", size: 40, action: addCard)
+            }
+            .padding(.vertical)
+        }
+        .padding()
+        .foregroundColor(.red)
+    }
+    
+    private func createButton(
+        _ image: String, size: CGFloat, action: @escaping (() -> Void)
+    ) -> some View {
+        return Button(
+            action: action,
+            label: {
+                Image(systemName: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: size, height: size)
+            }
+        )
+    }
+    
+    private func removeCard() {
+        if numberOfCards > 1 {
+            numberOfCards -= 1
+        }
+    }
+    
+    private func shuffleCards() {
+        emojis = emojis.shuffled()
+    }
+    
+    private func addCard() {
+        if numberOfCards < emojis.count {
+            numberOfCards += 1
+        }
+    }
+}
+
+struct CardView: View {
+    let icon: String
+    
+    @State private var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            let recShape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                recShape.fill().foregroundColor(.white)
+                recShape.strokeBorder(lineWidth: 3)
+                Text(icon).font(.largeTitle)
+            } else {
+                recShape.fill()
+            }
+        }.onTapGesture { isFaceUp.toggle() }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+.previewInterfaceOrientation(.portrait)
+        
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
